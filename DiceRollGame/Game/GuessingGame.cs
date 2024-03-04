152b875fa6cac403283d8f4910e11a053f9a3f01
jsj5909 <jsj5909@hotmail.com>
2024-03-04 11:42:35 -0800

    initial commit

---
+++ DiceRollGame/Game/GuessingGame.cs
@@ -0,0 +1,64 @@
+﻿using DiceRollGame.UserCommunication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRollGame.Game
+{
+
+
+    public class GuessingGame
+    {
+        private readonly Dice _dice;
+
+        private const int InitialTries = 3;
+
+        public GuessingGame(Dice dice)
+        {
+            _dice = dice;
+
+        }
+
+        public GameResult Play()
+        {
+            var diceRollResult = _dice.Roll();
+            Console.WriteLine($"Dice rolled. Guess what number it shows in {InitialTries} tries.");
+
+            var triesLeft = InitialTries;
+            while (triesLeft > 0)
+            {
+                var guess = ConsoleReader.ReadInteger("Enter a number:");
+
+                if (guess == diceRollResult)
+                {
+                    return GameResult.Victory;
+                }
+                Console.WriteLine("Wrong number.");
+                --triesLeft;
+
+            }
+            return GameResult.Loss;
+        }
+
+        internal static void PrintResult(GameResult gameResult)
+        {
+            string message = gameResult == GameResult.Victory ? "You win!" : "You Lose!"; ;
+
+            if (gameResult == GameResult.Victory)
+            {
+                message = "You Win!";
+            }
+            else
+            {
+                message = "You Lose";
+            }
+
+
+
+            Console.WriteLine(message);
+
+        }
+    }
+}
---
+++ DiceRollGame/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DiceRollGame.Game;
+
+Console.WriteLine("Hello, World!");
+
+
+var random = new Random(); 
+var dice = new Dice(random);
+var guessingGame = new GuessingGame(dice);
+
+GameResult gameResult = guessingGame.Play();
+
+GuessingGame.PrintResult(gameResult);
+
+
+Console.ReadKey();
